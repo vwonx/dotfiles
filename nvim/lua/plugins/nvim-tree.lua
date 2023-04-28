8639e8097ebd99b14d737a9b55005d1d514dc008
vwonx <vwonx@outlook.com>
2023-04-29 00:02:04 +0800

    Modify the kitty and nvim-tree configurations
    Signed-off-by: vwonx <vwonx@outlook.com>

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,42 +1,42 @@
 local keys = require("utils.global-keys-mapping").nvimTree
 
-local list_keys = {
-  { key = keys.refresh, action = "refresh" },
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
 
-  -- open / close --
-  { key = keys.edit, action = "edit" },
-  { key = keys.close, action = "close_node" },
-  { key = keys.system_open, action = "system_open" },
-  { key = keys.vsplit, action = "vsplit" },
-  { key = keys.split, action = "split" },
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
 
-  -- movement ---------------
-  { key = keys.parent_node, action = "parent_node" },
-  { key = keys.cd, action = "cd" },
-  { key = keys.dir_up, action = "dir_up" },
-  { key = keys.prev_sibling, action = "prev_sibling" },
-  { key = keys.next_sibling, action = "next_sibling" },
-  { key = keys.first_sibling, action = "first_sibling" },
-  { key = keys.last_sibling, action = "last_sibling" },
+  api.config.mappings.default_on_attach(bufnr)
 
-  -- file toggle --
-  { key = keys.toggle_git_ignored, action = "toggle_git_ignored" },
-  { key = keys.toggle_dotfiles, action = "toggle_dotfiles" },
-  { key = keys.toggle_custom, action = "toggle_custom" },
-  { key = keys.toggle_file_info, action = "toggle_file_info" },
-
-  ------ file operation ----------
-  { key = keys.create, action = "create" },
-  { key = keys.remove, action = "remove" },
-  { key = keys.rename, action = "rename" },
-  { key = keys.copy, action = "copy" },
-  { key = keys.cut, action = "cut" },
-  { key = keys.paste, action = "paste" },
-  { key = keys.copy_name, action = "copy_name" },
-  { key = keys.copy_path, action = "copy_path" },
-  { key = keys.copy_absolute_path, action = "copy_absolute_path" },
-  { key = keys.toggle_file_info, action = "toggle_file_info" },
-}
+  vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
+  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', '<CR>', api.node.run.system, opts('Run System'))
+  vim.keymap.set('n', 'sv', api.node.open.vertical, opts('Open: Vertical Split'))
+  vim.keymap.set('n', 'sh', api.node.open.horizontal, opts('Open: Horizontal Split'))
+  vim.keymap.set('n', 'P', api.node.navigate.parent, opts('Parent Directory'))
+  vim.keymap.set('n', ']', api.tree.change_root_to_node, opts('CD'))
+  vim.keymap.set('n', '[', api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', '<', api.node.navigate.sibling.prev, opts('Previous Sibling'))
+  vim.keymap.set('n', '>', api.node.navigate.sibling.next, opts('Next Sibling'))
+  vim.keymap.set('n', 'K', api.node.navigate.sibling.first, opts('First Sibling'))
+  vim.keymap.set('n', 'J', api.node.navigate.sibling.last, opts('Last Sibling'))
+  vim.keymap.set('n', 'i', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
+  vim.keymap.set('n', '.', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
+  vim.keymap.set('n', 'u', api.tree.toggle_custom_filter, opts('Toggle Hidden'))
+  vim.keymap.set('n', 'gh', api.node.show_info_popup, opts('Info'))
+  vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
+  vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
+  vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
+  vim.keymap.set('n', 'c', api.fs.copy.node, opts('Copy'))
+  vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
+  vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
+  vim.keymap.set('n', 'y', api.fs.copy.filename, opts('Copy Name'))
+  vim.keymap.set('n', 'Y', api.fs.copy.relative_path, opts('Copy Relative Path'))
+  vim.keymap.set('n', 'gy', api.fs.copy.absolute_path, opts('Copy Absolute Path'))
+  vim.keymap.set('n', 'gh', api.node.show_info_popup, opts('Info'))
+end
 
 return {{
   "nvim-tree/nvim-tree.lua",
@@ -52,12 +52,7 @@ return {{
         "DiffviewFiles",
       },
     },
-    view = {
-      mappings = {
-        custom_only = false,
-        list = list_keys,
-      },
-    },
+    on_attach = on_attach,
     renderer = {
       highlight_git = true,
       icons = {
